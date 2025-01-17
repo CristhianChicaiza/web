8f53d5f091748d237462b98a59570b594f1ac927
CristhianChicaiza <cchicaiza90@outlook.com>
2025-01-17 17:32:28 -0500

    se agrego app

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
-  imports: [],
+
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router:Router ) {
+  this.loginForm = this.formBuilder.group({
+    email: ['example@gmail.com', [Validators.required,Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+  }
+  ngOnInit(): void {
+  }
+
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+ 
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+login(){
+  if(this.loginForm.valid){
+    console.log("llamar al servicio de login");
+    this.router.navigateByUrl('/inicio');
+    this.loginForm.reset();
+}else{
+  alert("error al ingresar");
+}
+}
 
 }
